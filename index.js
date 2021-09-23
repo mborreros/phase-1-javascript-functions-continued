55dad6c59ede1067860aca3da6be7da42748333d
Maya Borrero <mlbsindia@gmail.com>
2021-09-23 12:23:14 -0400

    commit tree 33a122e567c7606c73b6360451ae5cea2ac49b6f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function mondayWork (activity = "go to the office") {
+    return "This Monday, I will " + activity + ".";
+}
+
+function saturdayFun (activity = "roller-skate") {
+    return "This Saturday, I want to " + activity + "!";
+}
+
+function wrapAdjective (str = "*"){
+    return function(name = "special") {
+       return `You are ${str}${name}${str}!`; 
+    }
+}
